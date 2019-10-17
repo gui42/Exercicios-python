7bf410696f660720ccce26fcd62171b5813069be
gui42 <guilherme.hermes182@gmail.com>
2019-10-17 16:39:15 -0300

    not quite working yet

--- madlibs/helper.py
+++ madlibs/helper.py
@@ -1,1 +1,42 @@
 #arquivo que contem funções do jogo madlib
+
+def openMad():
+    try:
+        with open("puzzle.txt") as f:
+            lines = f.read().splitlines()
+            return lines
+    except FileNotFoundError as e:
+        print(":(")
+
+def criaVar(mad):
+    varList = []
+    var = ""
+    part_of_var = False
+    for x in mad:
+        if x == "[":
+            part_of_var = True
+        if x == "]":
+            var += x
+            varList.append(var)
+            var = ""
+            part_of_var = False
+        if part_of_var:
+            var += x
+    return varList   
+
+def readMad(lines):
+    varL =[]
+    for x in lines:
+      varL += criaVar(x)
+    return varL 
+
+
+def changeMad(userin, varList, mad):
+    for line in mad:
+        for item in varList:
+            if item in line:
+                index = varList.index(item)
+                line=(line.replace(item, userin[index]))
+                varList.pop(index)
+                userin.pop(index)
+        print(line)
--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,1 +1,18 @@
 #arquivo que controla o jogo de madlibs
+import helper
+
+userin=[]
+
+#só abre o arquivo mesmo
+mad = helper.openMad()
+#cria a lista de variaveis
+varList = helper.readMad(mad)
+
+for x in varList:
+        end = (len(x)-1)
+        visual = x[1:end].lower()
+        y = input("please enter a\\n "+visual+" ")
+        userin.append(y.lower())
+
+print()
+helper.changeMad(userin, varList, mad)
